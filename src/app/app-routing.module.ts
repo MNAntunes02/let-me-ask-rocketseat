a97c00accfa719caca961f4718c940ce23d22c62
Alisson <alissonam17@gmail.com>
2023-07-27 14:11:01 -0300

    feat: rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SalaComponent } from './pages/sala/sala.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "sala", component: SalaComponent},
+  {path:'sala/:idgame',component: SalaComponent},
+  {path: "**" , redirectTo: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
