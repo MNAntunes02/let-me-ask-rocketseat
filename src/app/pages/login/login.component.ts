8a384bcb84bdb5c99e4dcb8abd3f70ccf7e32f58
Alisson <alissonam17@gmail.com>
2023-07-27 18:37:24 -0300

    feat: serviço de authenticação

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  constructor(
+    private authService: AuthService
+  ){
+
+  }
+
+  async googleSingIn(){
+    await this.authService.googleSingIn();
+  }
+
 
 }
