f24286b359191bf2d11bb285d52a6fc22f214df9
Alisson <alissonam17@gmail.com>
2023-07-27 18:38:30 -0300

    feat: acesso ao firestore

--- src/app/pages/sala/sala.component.ts
+++ src/app/pages/sala/sala.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DocumentData, Firestore, collection, collectionData, doc, getDoc } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
+
 
 @Component({
   selector: 'app-sala',
@@ -6,5 +11,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./sala.component.css']
 })
 export class SalaComponent {
+
+  idSala:string = '';
+  test:any;
+  testDoc:any;
+  salas$:Observable<DocumentData[]|any>;
+  sala$:any;
+  perguntas$:Observable<DocumentData[]|any>;
+
+  constructor(
+    private route : ActivatedRoute,
+    private authService : AuthService,
+    private firestore: Firestore,
+  ){
+
+    this.route.params.subscribe(
+      (params: any) => this.idSala = params['idsala']
+    )
+
+    const salasRef = collection(this.firestore, 'salas')
+    this.salas$ = collectionData(salasRef)
+    const salaRef = doc(salasRef,this.idSala)
+    const perguntasRef = collection(salaRef,'perguntas')
+    this.perguntas$ = collectionData(perguntasRef);
+    
+
+    
+    this.salas$.subscribe(async (element) => {
+      this.test = element;
+      this.testDoc = (await getDoc(salaRef)).data();
+      // console.log(this.test)
+      // console.log(this.testDoc)
+    })
+
+    // this.perguntas$.subscribe(async (element) => {
+    //   console.log(element)
+    // })
+
+    
+
+  }
+
+  logout(){
+    this.authService.logout()
+  }
+
 
 }
