d156cf2519cc0f7d1b074ffd67dc54d30d829bd7
Alisson <alissonam17@gmail.com>
2023-07-31 10:40:46 -0300

    style: criação de service ainda não implementada

---
+++ src/app/services/sala.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { DocumentData, Firestore, collection, collectionData, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SalaService {
+
+
+}
